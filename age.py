0d84ac2a41b7f469929d75175550a192fd04ac72
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-29 19:57:03 +0500

    main.24090490895898949674967976947649574957495749849649894764947694769467495749674957495458405045903593059305834958497648764867494764867123456789000000098765432111111111111112345678900000000000987654321

---
+++ age.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age: "))
+if age>=10:
+    if age>=10 and age<=20:
+        print("you are allowed to enter the class")
+    else:
+        print("you are not allowed to enter the class")
+else:
+    print("you are not allowed to enter the class")
